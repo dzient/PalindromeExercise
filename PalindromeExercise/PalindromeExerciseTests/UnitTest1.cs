68090da7c10601b447dcf7d96ea0401bd8bcafa7
dzient <34776686+dzient@users.noreply.github.com>
2022-07-27 01:31:40 -0400

    Add files via upload

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,40 @@
 using System;
 using Xunit;
-
-namespace PalindromeExerciseTests
+//-----------------------------------------------------------------
+// UnitTesting Exercise
+//
+// Name: David Zientara
+// Date: 7-26-2022
+// Comments: An exercise in using unit testing, part two
+// Made changes per the instructions
+//----------------------------------------------------------------
+namespace PalindromeExercise.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        // Made a decent sample of palindromes (true) and non-palindromes (false)
+        //[Fact]
+        [Theory]
+        [InlineData("racecar",true)]
+        [InlineData("Hurple", false)]
+        [InlineData("Flarble",false)]
+        [InlineData("NoXinNixon",true)]
+        [InlineData("repaper",true)]
+        [InlineData("No X In NiXon", true)]
+        [InlineData("This is not a palindrome",false)]
+        [InlineData("A man a plaN a CaNaL PanaMa", true)]
+        [InlineData("A man a plaN a CaNaL PanaMa!!", true)]
+        [InlineData("A man a plaN a CaNaL PanaMa, blah blah blah", false)]
+        [InlineData("Madam, I'm Adam", true)]
 
+
+        public void PalindromeTest(string str, bool expected)
+        {
+            // Here is where we test IsAPalindrome:
+            WordSmith Word = new WordSmith();
+            var actual = Word.IsAPalindrome(str);
+            Assert.Equal(expected,actual);
         }
+        
     }
 }
